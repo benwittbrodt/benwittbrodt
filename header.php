2deaf4163ec56de6319c175edd1eae53997ed5c0
benwittbrodt <benwittbrodt@gmail.com>
2020-05-06 09:06:53 -0400

    Initial setup of head/foot and index

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+  </head>
+<body>
+<header>
+<div data-collapse="medium" data-animation="default" data-duration="400" class="portfolio_nav w-nav">
+    <div class="w-container">
+        <a href="index.html" aria-current="page" class="w-nav-brand w--current">
+        <div class="logo-text portfolio">Ben<strong>Wittbrodt</strong></div>
+        </a>
+    <nav role="navigation" class="nav-menu w-nav-menu">
+        <a href="index.html" aria-current="page" class="nav-link_pic w-nav-link w--current">Home</a>
+        <a href="about.html" class="nav-link_pic w-nav-link">About</a>
+        <a href="photography/portfolio.html" class="nav-link_pic w-nav-link">Portfolio</a>
+        <a href="photography/packages.html" class="nav-link_pic w-nav-link">Packages</a>
+        <!-- <a href="#" data-ix="show-side-contact" class="nav-link_pic contact w-nav-link">Contact</a> -->
+    </nav>
+    <div class="menu-button w-nav-button">
+        <div class="icon-3 w-icon-nav-menu"></div>
+    </div>
+</div>
+  
+</header>
