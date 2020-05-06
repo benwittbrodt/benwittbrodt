e9ffe0b00df570efa5c1c7480f62153178f8b9e4
benwittbrodt <benwittbrodt@gmail.com>
2020-05-06 11:37:11 -0400

    Update header.php

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">WebFont.load({  google: {    families: ["Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic","Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]  }});</script>
-  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+    <script type="text/javascript">WebFont.load({  google: {    families: ["Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic","Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]  }});</script>
+    <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
 
   </head>
 <body>
@@ -26,19 +26,5 @@
         <div class="icon-3 w-icon-nav-menu"></div>
     </div>
 </div>
-<div class="side-contact-block" data-ix="move-right"><a href="#" data-ix="close-side-contact" class="close-link">CLOSE ✕</a>
-    <div class="form-wrapper">
-      <div class="contact-heading">Get in Touch</div>
-      <div class="contact-text">Thank you for your interest! Please fill out the form below so I can reach out.</div>
-      <div class="w-form">
-        <form id="email-form" name="email-form" data-name="Email Form"><input type="text" id="name-3" name="name-3" data-name="Name 3" placeholder="Enter your name" maxlength="256" required="" class="text-field w-input"><input type="email" id="email-3" name="email-3" data-name="Email 3" placeholder="Enter your email address" maxlength="256" required="" class="text-field w-input"><textarea id="Details-2" name="Details-2" placeholder="Your message or inquiry..." maxlength="5000" data-name="Details 2" required="" class="text-field text-area w-input"></textarea><input type="submit" value="Send" data-wait="Please wait..." class="submit-button w-button"></form>
-        <div class="success-message w-form-done">
-          <p class="success-text">Thank you! Your submission has been received!</p>
-        </div>
-        <div class="w-form-fail">
-          <p>Oops! Something went wrong while submitting the form</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  
 </header>
