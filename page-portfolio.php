cd09cdd37cb51e796d3f54e316c25cc0173a8bcd
benwittbrodt <benwittbrodt@gmail.com>
2020-05-07 08:32:50 -0400

    create people portfolio page and update portfolio link

--- page-portfolio.php
+++ page-portfolio.php
@@ -3,23 +3,23 @@
 <div class="container-5 w-container">
     <div class="row-5 w-row">
         <div class="w-col w-col-6">
-          <a href="../photography/people.html" class="w-inline-block">
+          <a href="<?php echo site_url('/portfolio/people');?>" class="w-inline-block">
           <img src="<?php echo get_theme_file_uri('/images/people-link.jpg');?>" style="width: 1500px"> 
           <div class="tile-text">People</div>
           </a>
         </div>
 
         <div class="w-col w-col-6">
-          <a href="../photography/places.html" class="w-inline-block">
+          <a href="<?php echo site_url('/portfolio/places');?>" class="w-inline-block">
           <img src="<?php echo get_theme_file_uri('/images/places-link.jpg');?>" style="width: 1500px">
-          <div class="tile-text">PLACES</div>
+          <div class="tile-text">Places</div>
           </a>
         </div>
     </div>
     
     <div class="row-4 w-row">
       <div class="w-col w-col-6">
-        <a href="../photography/puppies.html" class="w-inline-block">
+        <a href="<?php echo site_url('/portfolio/puppies');?>" class="w-inline-block">
         <img src="<?php echo get_theme_file_uri('/images/puppies-link.jpg');?>" style="width: 1500px">
         <div class="tile-text">Puppies</div>
         </a>
