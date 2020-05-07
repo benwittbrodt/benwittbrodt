28d21066a182deee0a40fdc522334a9787641f2e
benwittbrodt <benwittbrodt@gmail.com>
2020-05-07 16:00:33 -0400

    Finish contact page and update link to contact on packages page

--- page-contact.php
+++ page-contact.php
@@ -1,14 +1,17 @@
 <?php get_header(); ?>
 
-<?php while ( have_posts() ) {
-    the_post();?>
-    <div class="w-container">
-    <?php the_content();
-    ?>
+<div class="w-container">
+    <div class="w-col w-col-6">
+        <br><p>Please reach out via this contact form and I will get back to you regarding your question, comment, or message. Let's create something together!<br>In the meantime connect with me on social media so you can get to know me a little better</p>
     </div>
-    <?php
-}
+    <div class="w-col w-col-6">
+    <?php while ( have_posts() ) {
+        the_post();
+        
+        the_content();
+    }?>
 
-?>
-
+    </div>
+    
+</div>
 <?php get_footer(); ?>
