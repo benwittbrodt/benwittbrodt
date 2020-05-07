991e6e4e5719e60fedb90f9e7a6ac7c3fd78b5fb
benwittbrodt <benwittbrodt@gmail.com>
2020-05-07 11:06:23 -0400

    fill in places portfolio

---
+++ page-places.php
@@ -0,0 +1,135 @@
+<?php get_header(); ?>
+
+<div class="div-block-12"><a href="#" class="lightbox-link w-inline-block w-lightbox"><img src="../images/20170923-76-Pano.jpg" width="1250" srcset="<?php echo get_theme_file_uri('/images/20170923-76-Pano.jpg');?>" class="image-14"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "type": "image",
+      "_id": "5ab322923c0d5f20ee6281da",
+      "fileName": "5ab1d286235c966e97b468ad_20170923-76-Pano.jpg",
+      "origFileName": "20170923-76-Pano.jpg",
+      "width": 2500,
+      "height": 871,
+      "fileSize": 1644705,
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20170923-76-Pano.jpg"
+    }
+  ],
+  "group": "Places"
+}</script></a>
+    <div class="w-row">
+      <div class="column-2 w-col w-col-6"><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180327-249-HDR.jpg');?>" alt="" class="image-8"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "width": 3000,
+      "height": 2000,
+      "fileName": "20180327-249-HDR.jpg",
+      "origFileName": "20180327-249-HDR.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180327-249-HDR.jpg",
+      "_id": "5af3b424d6bc36f5a21890ce",
+      "type": "image",
+      "fileSize": 1989070
+    }
+  ],
+  "group": "Places"
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/NYC_wm.jpg');?>" alt="" class="image-8"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "type": "image",
+      "_id": "5ab322923c0d5ff9e36281d4",
+      "fileName": "5a245209a1816d000132a4be_NYC_wm.jpg",
+      "origFileName": "NYC_wm.jpg",
+      "width": 2400,
+      "height": 1600,
+      "fileSize": 1844937,
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/NYC_wm.jpg"
+    }
+  ],
+  "group": "Places"
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20170811-7-228-Pano.jpg');?>" class="image-9"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "type": "image",
+      "_id": "5ab322923c0d5f1b3f6281d5",
+      "fileName": "5ab1d15e235c962191b46876_20170811-7-228-Pano.jpg",
+      "origFileName": "20170811-7-228-Pano.jpg",
+      "width": 2499,
+      "height": 1223,
+      "fileSize": 2597725,
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20170811-7-228-Pano.jpg"
+    }
+  ],
+  "group": "Places"
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20171221-33.jpg');?>" class="image-10"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "type": "image",
+      "_id": "5ab322923c0d5f54f66281d6",
+      "fileName": "5ab1d192c15bc698437d2ea9_20171221-33.jpg",
+      "origFileName": "20171221-33.jpg",
+      "width": 2500,
+      "height": 1667,
+      "fileSize": 3116657,
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20171221-33.jpg"
+    }
+  ],
+  "group": "Places"
+}</script></a></div>
+      <div class="column-3 w-col w-col-6"><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20171116-12.jpg');?>" class="image-11"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "type": "image",
+      "_id": "5ab322923c0d5f3abb6281dd",
+      "fileName": "5ab1d347235c9679a0b468e8_20171116-12.jpg",
+      "origFileName": "20171116-12.jpg",
+      "width": 2000,
+      "height": 2500,
+      "fileSize": 3494606,
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/vimages/20171116-12.jpg"
+    }
+  ],
+  "group": "Places"
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20170921-188-HDR.jpg');?>" class="image-12"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "type": "image",
+      "_id": "5ab322923c0d5fc0166281db",
+      "fileName": "5ab1d2b4eddf7920ea8362cd_20170921-188-HDR.jpg",
+      "origFileName": "20170921-188-HDR.jpg",
+      "width": 2500,
+      "height": 1667,
+      "fileSize": 3264643,
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20170921-188-HDR.jpg"
+    }
+  ],
+  "group": "Places"
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20170825-3-2000.jpg');?>" class="image-13"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "type": "image",
+      "_id": "5ab322923c0d5fb0ca6281dc",
+      "fileName": "5ab1d2da214c0c3732960d03_20170825-3 2000.jpg",
+      "origFileName": "20170825-3 2000.jpg",
+      "width": 2000,
+      "height": 1500,
+      "fileSize": 3467641,
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20170825-3-2000.jpg"
+    }
+  ],
+  "group": "Places"
+}</script></a></div>
+    </div><a href="#" class="lightbox-link w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180411-23-Pano-1.jpg');?>" size="width: 1250px" class="image-14"><script type="application/json" class="w-json">{
+  "items": [
+    {
+      "width": 4000,
+      "height": 1873,
+      "fileName": "20180411-23-Pano (1).jpg",
+      "origFileName": "20180411-23-Pano (1).jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180411-23-Pano-1.jpg",
+      "_id": "5af3b39d2d1960061e77a8e5",
+      "type": "image",
+      "fileSize": 3248976
+    }
+  ],
+  "group": "Places"
+}</script></a></div>
+
+<?php wp_footer(); ?>
