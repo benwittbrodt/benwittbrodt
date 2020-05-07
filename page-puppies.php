abadeef63da66eba81804e2de414518b8a9a32c9
benwittbrodt <benwittbrodt@gmail.com>
2020-05-07 11:21:51 -0400

    update puppies portfolio

--- page-puppies.php
+++ page-puppies.php
@@ -2,49 +2,49 @@
 
   <div class="div-block-12">
     <div class="row-2 w-row">
-      <div class="column-2 w-col w-col-7"><a href="#" class="portfolio_lb people w-inline-block w-lightbox"><img src="../images/20180513-80.jpg" width="1500" srcset="../images/20180513-80-p-500.jpeg 500w, ../images/20180513-80-p-1080.jpeg 1080w, ../images/20180513-80-p-1600.jpeg 1600w, ../images/20180513-80-p-2000.jpeg 2000w, ../images/20180513-80.jpg 2500w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 50vw, (max-width: 4545px) 55vw, 2500px" alt="" class="image-8"><script type="application/json" class="w-json">{
+      <div class="column-2 w-col w-col-7"><a href="#" class="portfolio_lb people w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180513-80.jpg');?>" style="width: 1500px" alt="" class="image-8"><script type="application/json" class="w-json">{
   "items": [
     {
       "width": 2500,
       "height": 1667,
       "fileName": "5afaee69e6e4ad518d0b7cff_20180513-80.jpg",
       "origFileName": "20180513-80.jpg",
-      "url": "../images/20180513-80.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180513-80.jpg",
       "_id": "5afaee69e6e4ad518d0b7cff",
       "type": "image",
       "fileSize": 1582604
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb people w-inline-block w-lightbox"><img src="../images/20180512-90.jpg" width="1500" srcset="../images/20180512-90-p-500.jpeg 500w, ../images/20180512-90-p-1080.jpeg 1080w, ../images/20180512-90-p-1600.jpeg 1600w, ../images/20180512-90-p-2000.jpeg 2000w, ../images/20180512-90.jpg 2500w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 50vw, (max-width: 4545px) 55vw, 2500px" alt="" class="image-9"><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb people w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180512-90.jpg');?>" style="width: 1500px" class="image-9"><script type="application/json" class="w-json">{
   "items": [
     {
       "width": 2500,
       "height": 1667,
       "fileName": "20180512-90.jpg",
       "origFileName": "20180512-90.jpg",
-      "url": "../images/20180512-90.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180512-90.jpg",
       "_id": "5afaef1f390d197af70fa30c",
       "type": "image",
       "fileSize": 1570648
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20180512-118.jpg" width="1500" srcset="../images/20180512-118-p-500.jpeg 500w, ../images/20180512-118-p-1080.jpeg 1080w, ../images/20180512-118-p-1600.jpeg 1600w, ../images/20180512-118-p-2000.jpeg 2000w, ../images/20180512-118.jpg 2500w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 50vw, (max-width: 4545px) 55vw, 2500px" alt="" class="image-10"><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180512-118.jpg');?>" style="width: 1500px" class="image-10"><script type="application/json" class="w-json">{
   "items": [
     {
       "width": 2500,
       "height": 1667,
       "fileName": "20180512-118.jpg",
       "origFileName": "20180512-118.jpg",
-      "url": "../images/20180512-118.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180512-118.jpg",
       "_id": "5afaefab390d19e4da0fa36e",
       "type": "image",
       "fileSize": 1574930
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20180714-926.jpg" width="1500" srcset="../images/20180714-926-p-1080.jpeg 1080w, ../images/20180714-926-p-1600.jpeg 1600w, ../images/20180714-926-p-2000.jpeg 2000w, ../images/20180714-926.jpg 2500w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 50vw, (max-width: 4545px) 55vw, 2500px" alt="" class="image-10"><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180714-926.jpg');?>" style="width: 1500px" alt="" class="image-10"><script type="application/json" class="w-json">{
   "items": [
     {
       "type": "image",
@@ -54,11 +54,11 @@
       "width": 2500,
       "height": 1667,
       "fileSize": 871568,
-      "url": "../images/20180714-926.jpg"
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180714-926.jpg"
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20180714-257.jpg" width="1500" srcset="../images/20180714-257-p-1080.jpeg 1080w, ../images/20180714-257-p-1600.jpeg 1600w, ../images/20180714-257-p-2000.jpeg 2000w, ../images/20180714-257.jpg 2500w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 50vw, (max-width: 4545px) 55vw, 2500px" alt="" class="image-10"><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180714-257.jpg');?>" style="width: 1500px" alt="" class="image-10"><script type="application/json" class="w-json">{
   "items": [
     {
       "type": "image",
@@ -68,12 +68,12 @@
       "width": 2500,
       "height": 1667,
       "fileSize": 814079,
-      "url": "../images/20180714-257.jpg"
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180714-257.jpg"
     }
   ],
   "group": "Puppies"
 }</script></a></div>
-      <div class="column-3 w-col w-col-5"><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20190316-27.jpg" width="1500" srcset="../images/20190316-27-p-500.jpeg 500w, ../images/20190316-27-p-800.jpeg 800w, ../images/20190316-27-p-1080.jpeg 1080w, ../images/20190316-27-p-1600.jpeg 1600w, ../images/20190316-27.jpg 1669w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 36vw, (max-width: 4392px) 38vw, 1669px" alt="" class="image-10"><script type="application/json" class="w-json">{
+      <div class="column-3 w-col w-col-5"><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20190316-27.jpg');?>" style="width: 1500px" alt="" class="image-10"><script type="application/json" class="w-json">{
   "items": [
     {
       "type": "image",
@@ -83,39 +83,39 @@
       "width": 1669,
       "height": 2500,
       "fileSize": 1645716,
-      "url": "../images/20190316-27.jpg"
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20190316-27.jpg"
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20180513-58.jpg" width="1200" srcset="../images/20180513-58-p-500.jpeg 500w, ../images/20180513-58-p-800.jpeg 800w, ../images/20180513-58-p-1080.jpeg 1080w, ../images/20180513-58-p-1600.jpeg 1600w, ../images/20180513-58-p-2000.jpeg 2000w, ../images/20180513-58.jpg 2500w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 36vw, (max-width: 6410px) 39vw, 2500px" alt="" class="image-11"><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180513-58.jpg');?>" style="width: 1200px" alt="" class="image-11"><script type="application/json" class="w-json">{
   "items": [
     {
       "width": 2500,
       "height": 1667,
       "fileName": "20180513-58.jpg",
       "origFileName": "20180513-58.jpg",
-      "url": "../images/20180513-58.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180513-58.jpg",
       "_id": "5afaeeef5d83d921158cd078",
       "type": "image",
       "fileSize": 1926817
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20180519-163-Edit_1.jpg" width="1200" srcset="../images/20180519-163-Edit_1-p-500.jpeg 500w, ../images/20180519-163-Edit_1-p-800.jpeg 800w, ../images/20180519-163-Edit_1-p-1080.jpeg 1080w, ../images/20180519-163-Edit_1-p-1600.jpeg 1600w, ../images/20180519-163-Edit_1-p-2000.jpeg 2000w, ../images/20180519-163-Edit_1.jpg 2500w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 36vw, (max-width: 6410px) 39vw, 2500px" alt="" class="image-11"><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180519-163-Edit_1.jpg');?>" style="width: 1200px" class="image-11"><script type="application/json" class="w-json">{
   "items": [
     {
       "width": 2500,
       "height": 1667,
       "fileName": "20180519-163-Edit.jpg",
       "origFileName": "20180519-163-Edit.jpg",
-      "url": "../images/20180519-163-Edit_1.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180519-163-Edit_1.jpg",
       "_id": "5b0f41bfeb261d602abbd83f",
       "type": "image",
       "fileSize": 1324940
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20181117-9.jpg" width="834.5" srcset="../images/20181117-9-p-500.jpeg 500w, ../images/20181117-9-p-800.jpeg 800w, ../images/20181117-9-p-1080.jpeg 1080w, ../images/20181117-9-p-1600.jpeg 1600w, ../images/20181117-9.jpg 1669w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 36vw, 39vw" alt=""><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20181117-9.jpg');?>" style="width: 834.5px" alt=""><script type="application/json" class="w-json">{
   "items": [
     {
       "width": 1669,
@@ -123,21 +123,21 @@
       "height": 2500,
       "fileName": "20181117-9.jpg",
       "origFileName": "20181117-9.jpg",
-      "url": "../images/20181117-9.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20181117-9.jpg",
       "_id": "5c78752f07fa9540e2e9e9c1",
       "type": "image",
       "fileSize": 1489295
     }
   ],
   "group": "Puppies"
-}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="../images/20180512-15.jpg" width="1200" srcset="../images/20180512-15-p-500.jpeg 500w, ../images/20180512-15-p-800.jpeg 800w, ../images/20180512-15-p-1080.jpeg 1080w, ../images/20180512-15-p-1600.jpeg 1600w, ../images/20180512-15.jpg 2000w" sizes="(max-width: 479px) 91vw, (max-width: 767px) 82vw, (max-width: 991px) 36vw, (max-width: 5128px) 39vw, 2000px" alt="" class="image-11"><script type="application/json" class="w-json">{
+}</script></a><a href="#" class="portfolio_lb w-inline-block w-lightbox"><img src="<?php echo get_theme_file_uri('/images/20180512-15.jpg');?>" style="width: 1200px" alt="" class="image-11"><script type="application/json" class="w-json">{
   "items": [
     {
       "width": 2000,
       "height": 2500,
       "fileName": "20180512-15.jpg",
       "origFileName": "20180512-15.jpg",
-      "url": "../images/20180512-15.jpg",
+      "url": "//localhost:3000/wp-content/themes/benwittbrodt-theme/images/20180512-15.jpg",
       "_id": "5afaef057a74dd4e5ecae10f",
       "type": "image",
       "fileSize": 1812568
