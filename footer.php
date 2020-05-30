db806dfde13213d87914e68fdbdfc905f69a1989
benwittbrodt <benwittbrodt@gmail.com>
2020-05-30 10:05:29 -0400

    update css for index buttons

--- footer.php
+++ footer.php
@@ -2,16 +2,16 @@
   <div class="w-container">
     <div class="w-row">
       <div class="w-col w-col-3">
-        <a href="<?php echo site_url('/'); ?>" aria-current="page" class="logo-footer w-nav-brand w--current">
+        <a href="<?php echo site_url('/'); ?>" aria-current="page" class="logo-footer w-nav-brand">
           <div class="logo-text footer-logo">Ben<strong>Wittbrodt</strong></div>
         </a>
       </div>
-      <div class="footer-link-col w-clearfix w-col w-col-9">
+      <div class="w-col w-col-9">
         <div class="social-icons-footer">
-        <a href="https://www.instagram.com/benwittbrodt/" target="_blank"><i class="fab fa-instagram footer_link"></i></a>
-        <a href="https://www.facebook.com/benwittbrodt" target="_blank"><i class="fab fa-facebook footer_link"></i></a>
-        <a href="http://www.linkedin.com/in/benwittbrodt" target="_blank"><i class="fab fa-linkedin-in footer_link"></i></a>
-        <a href="https://www.github.com/benwittbrodt" target="_blank"><i class="fab fa-github footer_link"></i></a>
+          <a href="https://www.instagram.com/benwittbrodt/" target="_blank"><i class="fab fa-instagram footer_link"></i></a>
+          <a href="https://www.facebook.com/benwittbrodt" target="_blank"><i class="fab fa-facebook footer_link"></i></a>
+          <a href="http://www.linkedin.com/in/benwittbrodt" target="_blank"><i class="fab fa-linkedin-in footer_link"></i></a>
+          <a href="https://www.github.com/benwittbrodt" target="_blank"><i class="fab fa-github footer_link"></i></a>
         </div>
         
       </div>
@@ -20,7 +20,7 @@
   </div>
 </div>
 <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js?site=58ff3b84d37f1011096e8281" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script src="js/webflow.js" type="text/javascript"></script>
+<script src="js/webflow.js" type="text/javascript"></script>
 
 <?php wp_footer(); ?>
 
