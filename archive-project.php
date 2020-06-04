4f7bae3107e4d6269ec06c87983cecbf41b60f5b
benwittbrodt <benwittbrodt@gmail.com>
2020-06-04 14:27:02 -0400

    Create iframe CSS for the post and finish styling the "card" entry for project archive

--- archive-project.php
+++ archive-project.php
@@ -1,10 +1,23 @@
 <?php get_header(); ?>
 
+<div class="w-container">
+
+
 <?php 
     while ( have_posts() ) {
         the_post();
-        echo the_title();
+?>   
+        <a style="color: black" href="<?php echo get_the_permalink();?>">
+        <div class="card w-col w-col-3">
+            <div class="center">
+            <?php the_post_thumbnail( 'thumbnail' );?>
+            </div>
+            <h2 class="center"><?php the_title();?></h2>
+        </div>
+        </a>
+<?php    
     }
 ?>
-
+</div>
+<div class="div-block-16"></div>
 <?php get_footer(); ?>
