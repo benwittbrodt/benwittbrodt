288dad9f12d0a91c7fcbf563edff206765cf9d1f
benwittbrodt <benwittbrodt@gmail.com>
2020-06-08 18:51:13 -0400

    Link in "publication" post tag for research projects

--- archive-background.php
+++ archive-background.php
@@ -24,12 +24,22 @@
             <?php } wp_reset_postdata();?>
 
         <h1 class="background_heading">Research</h1>
-          <div class="w-richtext">
-            <p>Life-cycle economic analysis of distributed manufacturing with open-source 3-D printers</p>
-            <p>B.T. Wittbrodt, A.G. Glover, J. Laureto, G.C. Anzalone, D. Oppliger, J. L. Irwin, J.M. Pearce,Mechatronics, 23, pp. 713-726</p>
-            <p>‍<a href="http://bit.ly/1jGmOBJ">Open Access Link</a></p>
-            <p><a href="http://bit.ly/1wFRns0">Gained U.S. and international press attention</a><br></p>
-          </div>
+        <?php
+            $research_query = new WP_Query( array( 
+                'post_type'=> 'background',
+                'category_name' => 'publication', 
+                'order' => 'ASC',
+                'orderby' => 'ID'
+                ) );
+
+            while ( $research_query->have_posts() ) {
+                $research_query->the_post();?>
+                <div class="w-richtext">
+                <h4><?php the_title();?></h4>
+                <?php 
+                the_content();  ?>
+            </div>
+            <?php } wp_reset_postdata();?>
         </div>
         <div class="w-col w-col-6">
         <h1 class="background_heading">Experience</h1>
