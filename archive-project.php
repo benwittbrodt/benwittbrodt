07e8302f61c5f56a5dc26890e7af6d7572d86952
Ben Wittbrodt <benwitt08@gmail.com>
2020-12-09 18:53:32 -0500

    uploaded wp and arduino icons and called icons in post and archive

--- archive-project.php
+++ archive-project.php
@@ -1,32 +1,35 @@
 <?php get_header(); ?>
 
-<h2 class="center archive-heading">All Web Projects</h2>
+<h2 class="center archive-heading">All Projects</h2>
     <div class="w-container">
-    <div>
-    <p>A brief highlight of web based projects and some design related projects that I have been working on. If you are looking to revamp your online presence and need something from a single landing page to a whole website redesign and software asset package to match, reach out via my contact page and we can discuss your project further!</p>
-   <a href="<?php echo site_url('contact');?>"><button class="index-button">Contact Me</button></a>
-    </div>
+        <div>
+            <p>A brief highlight of web based projects and some design related projects that I have been working on. If you are looking to revamp your online presence and need something from a single landing page to a whole website redesign and software asset package to match, reach out via my contact page and we can discuss your project further!</p>
+            <a href="<?php echo site_url('contact');?>"><button class="index-button">Contact Me</button></a>
+        </div>
         <?php 
-        while ( have_posts() ) {
+        while( have_posts() ) {
             the_post();
-        ?>   
+            ?>   
+            <!-- Creating the "card" for each project - eeverything within the anchor tag so the whole card is clickable -->
             <a style="color: black" href="<?php echo get_the_permalink();?>">
-            <div class="card w-col w-col-3">
-                <div class="center">
-                    <?php the_post_thumbnail( 'thumbnail' );?>
+                <div class="card w-col w-col-3">
+                    <div class="center">
+                        <?php the_post_thumbnail( 'thumbnail' );?>
+                    </div>
+                    <h2 class="center"><?php the_title();?></h2>
+                    <?php 
+                        //Gettign the "technologies" terms associated with each post
+                        $term_obj_list = get_the_terms( $post->ID, 'technologies' );
+                
+                        foreach($term_obj_list as $key) {
+                            $icon = $key->slug;
+                            ?>
+                            <!-- sourcing icon for each technology type -->
+                            <img style="height: 90px" src="<?php echo get_theme_file_uri('images/icons/' . $icon . '_small.png');?>" alt="">
+                            <?php
+                        }
+                    ?>
                 </div>
-                <h2 class="center"><?php the_title();?></h2>
-                <?php 
-                $term_obj_list = get_the_terms( $post->ID, 'technologies' );
-             
-                foreach($term_obj_list as $key) {
-                $icon = $key->slug;
-                ?>
-                <i style="color: black" class="fab fa-<?php echo $icon;?>"></i>
-                <?php
-                }
-                ?>
-            </div>
             </a>
         <?php    
         }
--- single-project.php
+++ single-project.php
@@ -1,20 +1,26 @@
 <?php get_header(); ?>
 <div class="center post-heading">
-<div style="margin-right: 5px"><?php the_post_thumbnail('homeLogo');?></div> <h2> <?php the_title();?></h2>
+    <div style="margin-right: 5px"><?php the_post_thumbnail('homeLogo');?></div> 
+    <h2><?php the_title();?></h2>
 </div>
 <div class="w-container">
-
     <?php 
         while ( have_posts() ) {
             the_post();
             
             //Get list of all taxonomy terms for the given post ID
-            $term_obj_list = get_the_terms( $post->ID, 'technologies' );
-            print_r($term_obj_list);
+            $term_obj_list = get_the_terms( $post->ID, 'technologies' ); 
+                
+                foreach($term_obj_list as $key) {
+                    $icon = $key->slug;
+                    ?>
+                    <!-- sourcing icon for each technology type -->
+                    <img style="height: 90px" src="<?php echo get_theme_file_uri('images/icons/' . $icon . '_small.png');?>" alt="">
+                    <?php
+                }
             the_content();
         }
     ?>
-
 </div>
 
 <?php get_footer(); ?>
