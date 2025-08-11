4dbd08251e3618d9f538741cd49eb580bc4ec84d
Ben Wittbrodt <benwitt08@gmail.com>
2025-08-10 21:50:17 -0500

    add apollocare

--- archive-background.php
+++ archive-background.php
@@ -3,20 +3,21 @@
 <div class="container">
     <div class="container mt-5">
         <h2><?php echo str_replace('Archives:','My',get_the_archive_title());?></h2>
-               
-        <div class="row gx-5">
-        <div class="col-lg-8"> 
-            <!-- Professional Experience -->
-            <?php background_archive('sermo', 'experience','start_date'); ?>
-            <?php background_archive('continental', 'experience','start_date'); ?>
-            <?php background_archive('michigan-tech', 'experience','start_date'); ?>
-            <?php background_archive('cummins', 'experience','start_date'); ?>
 
-        </div>
-        <div class="col-lg-4">
-            <!-- Education -->
-            <?php background_archive('michigan-tech','education','end_date'); ?>
-        </div>
+        <div class="row gx-5">
+            <div class="col-lg-8">
+                <!-- Professional Experience -->
+                <?php background_archive('apollocare', 'experience','start_date'); ?>
+                <?php background_archive('sermo', 'experience','start_date'); ?>
+                <?php background_archive('continental', 'experience','start_date'); ?>
+                <?php background_archive('michigan-tech', 'experience','start_date'); ?>
+                <?php background_archive('cummins', 'experience','start_date'); ?>
+
+            </div>
+            <div class="col-lg-4">
+                <!-- Education -->
+                <?php background_archive('michigan-tech','education','end_date'); ?>
+            </div>
         </div>
     </div>
 </div>
